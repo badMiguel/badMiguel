4f055f7162dd29982907ba11cd997e185fbb5b97
MiguelBad <badayosbiboy@gmail.com>
2025-11-03 08:25:10 +0930

    added colors in js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-function main() { }
+const colors = {
+    darkBg1: "#191724",
+    darkBg2: "#1f1d2e",
+    darkBg3: "#26233a",
+    darkFont1: "#e0def4",
+    darkFont2: "#6e6a86",
+    darkFont3: "#908caa",
+    darkLove: "#eb6f92",
+    darkGold: "#f6c177",
+    darkRose: "#ebbcba",
+    darkPine: "#31748f",
+    darkFoam: "#9ccfd8",
+    darkIris: "#c4a7e7",
+    darkHighlight1: "#21202e",
+    darkHighlight2: "#403d52",
+    darkHighlight3: "#524f67",
+
+    lightBg1: "#faf4ed",
+    lightBg2: "#fffaf3",
+    lightBg3: "#f2e9e1",
+    lightFont1: "#9893a5",
+    lightFont2: "#797593",
+    lightFont3: "#575279",
+    lightLove: "#b4637a",
+    lightGold: "#ea9d34",
+    lightRose: "#d7827e",
+    lightPine: "#286983",
+    lightFoam: "#56949f",
+    lightIris: "#907aa9",
+    lightHighlight1: "#f4ede9",
+    lightHighlight2: "#dfdad9",
+    lightHighlight3: "#cecacd",
+};
+
+function main() {
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     main();
