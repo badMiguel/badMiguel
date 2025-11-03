9b55f85e67401ed6ace50b5069608e0b09499670
MiguelBad <badayosbiboy@gmail.com>
2025-11-03 09:30:50 +0930

    simple theme change when scrolling

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ const colors = {
     lightBg1: "#faf4ed",
     lightBg2: "#fffaf3",
     lightBg3: "#f2e9e1",
-    lightFont1: "#9893a5",
+    lightFont1: "#575279",
     lightFont2: "#797593",
-    lightFont3: "#575279",
+    lightFont3: "#9893a5",
     lightLove: "#b4637a",
     lightGold: "#ea9d34",
     lightRose: "#d7827e",
@@ -32,7 +32,50 @@ const colors = {
     lightHighlight3: "#cecacd",
 };
 
+function changeTheme(theme) {
+    if (theme === "light") {
+        document.querySelector("body").style.backgroundColor = colors.lightBg1;
+        document.querySelector("body").style.color = colors.lightFont1;
+    } else {
+        document.querySelector("body").style.backgroundColor = colors.darkBg1;
+        document.querySelector("body").style.color = colors.darkFont1;
+    }
+}
+
+function getElementAbsoluteYPosition(element) {
+    return document.querySelector(element).getBoundingClientRect().top + window.scrollY;
+}
+
+function getElementHeight(element) {
+    return document.querySelector(element).clientHeight;
+}
+
+function scrollEffects() {
+    const lightGroup = [".home-container", ".tech-stack-container"];
+
+    document.addEventListener("scroll", () => {
+        const shouldLight = lightGroup.some((el) => {
+            const posY = getElementAbsoluteYPosition(el) - window.innerHeight / 2;
+            const posYEnd = posY + getElementHeight(el);
+
+            if (posY <= window.scrollY && posYEnd >= window.scrollY) {
+                return true;
+            } else {
+                console.log({ posY, posYEnd });
+                return false;
+            }
+        });
+
+        if (shouldLight) {
+            changeTheme("light")
+        } else {
+            changeTheme("dark")
+        }
+    });
+}
+
 function main() {
+    scrollEffects();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
